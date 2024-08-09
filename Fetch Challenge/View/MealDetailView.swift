2096521dfa75aae8b200ed802a69e78908fbf0c5
Amine Slaoui <ms12597@lga-bsmt-01.sts.its.nyu.edu>
2024-08-09 17:46:12 -0400

    Updated UI for MealDetailView

--- Fetch Challenge/View/MealDetailView.swift
+++ Fetch Challenge/View/MealDetailView.swift
@@ -12,10 +12,25 @@ struct MealDetailView: View {
     @StateObject private var viewModel = MealDetailViewModel()
     
     var body: some View{
+        
         VStack {
             if let meal = viewModel.meal{
-                Text(meal.strMeal).font(.largeTitle)
+                Text(meal.strMeal)
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                
+                Text("Instructions")
+                    .font(.headline)
+                    .padding(.vertical, 3)
                 Text(meal.strInstructions)
+                    .font(.body)
+                    .padding(.bottom)
+                
+                
+                Text("Ingredients with measurements")
+                    .font(.headline)
+                    .padding(.vertical, 3)
                 List(meal.ingredients.keys.sorted(), id: \.self) {
                     key in if let value = meal.ingredients[key] {
                         Text("\(key): \(value)")
@@ -27,7 +42,8 @@ struct MealDetailView: View {
                 ProgressView()
             }
         }
-        .navigationTitle("Meal Details")
+        .padding(.horizontal, 4)
+        .navigationTitle("Dessert Details")
         .task {
             await viewModel.getMealDetails(id: mealID)
         }
